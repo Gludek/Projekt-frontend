64527688c6002ef2e791399de5dd64056612424d
Szymon Nadbrzezny <gludekpl@gmail.com>
2023-10-01 15:21:22 +0200

    fix for handleConfirmation

--- src/pages/HandleCofnrimation.tsx
+++ src/pages/HandleCofnrimation.tsx
@@ -1,9 +1,17 @@
 import { ApiClient } from "@/API/apiClient";
+import { useState } from "react";
 import { Navigate, useParams, useSearchParams } from "react-router-dom";
 
 function HandleCofnrimation() {
   const [params] = useSearchParams();
-  ApiClient.confirmRegistration(params.get("confirmation_token")!);
+  const [ready, setReady] = useState(false);
+  ApiClient.confirmRegistration(params.get("confirmation_token")!).then(
+    (res) => {
+      console.log(res);
+      setReady(true);
+    }
+  );
+  if (!ready) return <div>loading...</div>;
   return <Navigate to={"/"} replace={true} />;
 }
 
